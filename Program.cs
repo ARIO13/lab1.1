aa8aaded0666bfd8f892b713f78ada8f0ca9e331
ARIO13 <110298616+ARIO13@users.noreply.github.com>
2022-08-01 13:08:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace GAZ.CSCourse.TestConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;//create string
+            StreamReader sr = new StreamReader("c:\\users\\admin\\documents\\visual studio 2010\\Projects\\GAZ.CSCourse.TestConsoleApplication\\GAZ.CSCourse.TestConsoleApplication\\DataFile.txt");
+             
+             while ((line = sr.ReadLine()) != null) //enter line value
+            {
+                line = line.Replace(",","y:");//format
+                line = "x:" + line;//merge 
+                Console.WriteLine(line);//out line
+                            
+            }
+             
+
+        }
+    }
+}
